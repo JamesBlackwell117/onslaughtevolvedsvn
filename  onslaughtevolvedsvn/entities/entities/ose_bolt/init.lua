c553a031d465e2d06b749e56300c100c7275d2df
ailiamorisato <ailiamorisato@5b65d7ca-4450-0410-b60c-19f04ccc024d>
2008-07-18 03:07:13 +0000

    made sure xbow bolts always eventually removed themselves

---  onslaughtevolvedsvn/entities/entities/ose_bolt/init.lua
+++  onslaughtevolvedsvn/entities/entities/ose_bolt/init.lua
@@ -14,6 +14,7 @@ function ENT:Initialize()
 	self:SetMoveType(MOVETYPE_FLYGRAVITY)
 	self:SetGravity( 0.05 )
 	local trail = util.SpriteTrail(self, 0, Color(100,100,100), false, 5, 1, 4, 1/(15+1)*0.5, "trails/smoke.vmt")
+	timer.Simple(10,self.Remove,self)
 end
 
 function ENT:Touch(ent)
@@ -24,7 +25,6 @@ function ENT:Touch(ent)
 		self:Remove()
 		self.Touched = self.Touched + 1
 	elseif class == "sent_prop"|| class == "sent_ladder"|| class == "sent_ammo_dispenser" then
-		timer.Simple(5,self.Remove,self)
 		self.Touched = self.Touched + 1
 	end
 end
