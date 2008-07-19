a179838c7909e4bb83fad47fe2aaa56fbedcb477
conman420gb <conman420gb@5b65d7ca-4450-0410-b60c-19f04ccc024d>
2008-07-19 23:21:53 +0000

    Added .Owner clientside for sent_prop and derivers

---  onslaughtevolvedsvn/entities/entities/sent_prop/cl_init.lua
+++  onslaughtevolvedsvn/entities/entities/sent_prop/cl_init.lua
@@ -1,6 +1,13 @@
 include('shared.lua')
- 
+
 function ENT:Draw()             
 	self.Entity:DrawModel()
 end
- 
+
+function Owner(um)
+	local owner = um:ReadEntity()
+	local ent = um:ReadEntity()
+	ent.Owner = owner
+end
+
+usermessage.Hook("owner", Owner)
