a83aa7e9c3f387f39770c7e8429b0e5c4be72586
ailiamorisato <ailiamorisato@5b65d7ca-4450-0410-b60c-19f04ccc024d>
2008-07-23 05:27:18 +0000

    increased cost of fuel to compensate for better flamethrower

--- trunk/ onslaughtevolvedsvn/gamemode/shared.lua
+++ trunk/ onslaughtevolvedsvn/gamemode/shared.lua
@@ -60,7 +60,7 @@ AMMOS[6] = {AMMO = "AR2AltFire", NAME = "Combine Ball", SMULT = 2, QT = 1, PRICE
 AMMOS[7] = {AMMO = "xbowbolt", NAME = "Crossbow Bolt", SMULT = 2, QT = 10, PRICE = 500, MODEL = "models/Items/CrossbowRounds.mdl"}
 AMMOS[8] = {AMMO = "grenade", NAME = "Grenade", SMULT = 2, QT = 1, PRICE = 300, MODEL = "models/Items/grenadeAmmo.mdl"}
 AMMOS[9] = {AMMO = "SMG1_Grenade", NAME = "Smg Grenade", QT = 1, PRICE = 250, MODEL = "models/Items/AR2_Grenade.mdl"}
-AMMOS[10] = {AMMO = "AR2", NAME = "Fuel", SMULT = 2, QT = 100, PRICE = 200, MODEL = "models/props_junk/gascan001a.mdl"}
+AMMOS[10] = {AMMO = "AR2", NAME = "Fuel", SMULT = 2, QT = 100, PRICE = 500, MODEL = "models/props_junk/gascan001a.mdl"}
 AMMOS[11] = {AMMO = "BuckShot", NAME = "Heavy Buckshot", QT = 32, PRICE = 200, MODEL = "models/Items/BoxFlares.mdl"}
 AMMOS[12] = {AMMO = "SMG1", NAME = "Mine", SMULT = 2, QT = 1, PRICE = 300, MODEL = "models/props_combine/combine_mine01.mdl"}
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -247,7 +247,7 @@ end
 	DEATH_PENALTY = -1500
 
 	--PROP VARS--
-	FLAMABLE_PROPS = true
+	FLAMABLE_PROPS = false
 	BATTLE_PHYS = false
 	PROP_CLEANUP = false
 	PROP_LIMIT = 35
