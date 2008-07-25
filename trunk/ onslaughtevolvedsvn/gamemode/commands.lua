d36d6645cfd57bd6508a9fa4144a9886b77c380c
ailiamorisato <ailiamorisato@5b65d7ca-4450-0410-b60c-19f04ccc024d>
2008-07-25 03:44:58 +0000

    added PropOp check to mass prop removals

--- trunk/ onslaughtevolvedsvn/gamemode/commands.lua
+++ trunk/ onslaughtevolvedsvn/gamemode/commands.lua
@@ -217,7 +217,7 @@ function SellAll(ply,cmd,args)
 	end
 	local mon = 0
 	for k,v in pairs(ents.GetAll()) do
-		if v:IsProp() then
+		if v:PropOp(ply) then
 			mon = mon + v:PropRemove(true,true)
 		end
 	end
@@ -402,7 +402,7 @@ function DeleteModel(ply, cmd, args)
 	local monadd = 0
 	if #entz > 0 then
 		for k,v in pairs(entz) do
-			if v:IsProp() then
+			if v:PropOp(ply) then
 				monadd = monadd + v:PropRemove(true,true)
 			end
 		end
