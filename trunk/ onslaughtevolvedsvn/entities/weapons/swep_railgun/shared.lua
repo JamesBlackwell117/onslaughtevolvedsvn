669ee7edad7c8ff58b6388413e49ad96aa3d1b49
ailiamorisato <ailiamorisato@5b65d7ca-4450-0410-b60c-19f04ccc024d>
2008-08-04 03:13:29 +0000

    added railgun scope

--- trunk/ onslaughtevolvedsvn/entities/weapons/swep_railgun/shared.lua
+++ trunk/ onslaughtevolvedsvn/entities/weapons/swep_railgun/shared.lua
@@ -37,10 +37,78 @@ SWEP.Zoomed = false
 
 SWEP.LastReload = CurTime()
 
+SWEP.ScopeScale = 0.4
+
+
 function SWEP:Initialize( )
 	if SERVER then
 		self:SetWeaponHoldType( "ar2" )
 	end
+	
+		if CLIENT then
+	
+		-- We need to get these so we can scale everything to the player's current resolution.
+		local iScreenWidth = surface.ScreenWidth()
+		local iScreenHeight = surface.ScreenHeight()
+		
+		-- The following code is only slightly riped off from Night Eagle
+		-- These tables are used to draw things like scopes and crosshairs to the HUD.
+		self.ScopeTable = {}
+		self.ScopeTable.l = iScreenHeight*self.ScopeScale
+		self.ScopeTable.x1 = 0.5*(iScreenWidth + self.ScopeTable.l)
+		self.ScopeTable.y1 = 0.5*(iScreenHeight - self.ScopeTable.l)
+		self.ScopeTable.x2 = self.ScopeTable.x1
+		self.ScopeTable.y2 = 0.5*(iScreenHeight + self.ScopeTable.l)
+		self.ScopeTable.x3 = 0.5*(iScreenWidth - self.ScopeTable.l)
+		self.ScopeTable.y3 = self.ScopeTable.y2
+		self.ScopeTable.x4 = self.ScopeTable.x3
+		self.ScopeTable.y4 = self.ScopeTable.y1
+				
+		self.ParaScopeTable = {}
+		self.ParaScopeTable.x = 0.5*iScreenWidth - self.ScopeTable.l
+		self.ParaScopeTable.y = 0.5*iScreenHeight - self.ScopeTable.l
+		self.ParaScopeTable.w = 2*self.ScopeTable.l
+		self.ParaScopeTable.h = 2*self.ScopeTable.l
+		
+		self.ScopeTable.l = (iScreenHeight + 1)*self.ScopeScale -- I don't know why this works, but it does.
+
+		self.QuadTable = {}
+		self.QuadTable.x1 = 0
+		self.QuadTable.y1 = 0
+		self.QuadTable.w1 = iScreenWidth
+		self.QuadTable.h1 = 0.5*iScreenHeight - self.ScopeTable.l
+		self.QuadTable.x2 = 0
+		self.QuadTable.y2 = 0.5*iScreenHeight + self.ScopeTable.l
+		self.QuadTable.w2 = self.QuadTable.w1
+		self.QuadTable.h2 = self.QuadTable.h1
+		self.QuadTable.x3 = 0
+		self.QuadTable.y3 = 0
+		self.QuadTable.w3 = 0.5*iScreenWidth - self.ScopeTable.l
+		self.QuadTable.h3 = iScreenHeight
+		self.QuadTable.x4 = 0.5*iScreenWidth + self.ScopeTable.l
+		self.QuadTable.y4 = 0
+		self.QuadTable.w4 = self.QuadTable.w3
+		self.QuadTable.h4 = self.QuadTable.h3
+
+		self.LensTable = {}
+		self.LensTable.x = self.QuadTable.w3
+		self.LensTable.y = self.QuadTable.h1
+		self.LensTable.w = 2*self.ScopeTable.l
+		self.LensTable.h = 2*self.ScopeTable.l
+
+		self.CrossHairTable = {}
+		self.CrossHairTable.x11 = 0
+		self.CrossHairTable.y11 = 0.5*iScreenHeight
+		self.CrossHairTable.x12 = iScreenWidth
+		self.CrossHairTable.y12 = self.CrossHairTable.y11
+		self.CrossHairTable.x21 = 0.5*iScreenWidth
+		self.CrossHairTable.y21 = 0
+		self.CrossHairTable.x22 = 0.5*iScreenWidth
+		self.CrossHairTable.y22 = iScreenHeight
+		
+	end
+
+	
 end
 
 function SWEP:Deploy()
@@ -72,15 +140,16 @@ function SWEP:PrimaryAttack()
 			ent = trace.Entity
 			iter = iter + 1
 		end
+		if self:Clip1() == 0 then self:SetNWBool("zoom", false) self.Owner:SetFOV( 90, 0 ) self:Reload() end
 	end
 end
 
 function SWEP:SecondaryAttack( )
-	if !self.Zoomed then
+	if !self:GetNWBool("zoom") then
 		self.Owner:SetFOV( 20, 0 )
-		self.Zoomed = true	
+		self:SetNWBool("zoom", true)
 	else
-		self.Zoomed = false
+		self:SetNWBool("zoom", false)
 		self.Owner:SetFOV( 90, 0 ) 
 	end
 end
@@ -93,3 +162,39 @@ end
 --	return pos,ang
 --end
 
+function SWEP:DrawHUD()
+	if self:GetNWBool("zoom") then 
+		-- Draw the crosshair
+		-- surface.SetDrawColor(0, 0, 0, 150)
+		-- surface.DrawLine(self.CrossHairTable.x11,self.CrossHairTable.y11,self.CrossHairTable.x12,self.CrossHairTable.y12)
+		-- surface.DrawLine(self.CrossHairTable.x21,self.CrossHairTable.y21,self.CrossHairTable.x22,self.CrossHairTable.y22)
+		
+		-- Draw the cool parabolic sights
+		--if self.DrawParabolicSights then
+			surface.SetDrawColor(0, 0, 0, 220)
+			surface.SetTexture(surface.GetTextureID("jaanus/ep2snip_parascope"))
+			surface.DrawTexturedRect(self.ParaScopeTable.x,self.ParaScopeTable.y,self.ParaScopeTable.w,self.ParaScopeTable.h)
+		--end
+		-- Draw the lens
+		surface.SetDrawColor(20,20,20,40)
+		surface.SetTexture(surface.GetTextureID("overlays/scope_lens"))
+		surface.DrawTexturedRect(self.LensTable.x,self.LensTable.y,self.LensTable.w,self.LensTable.h)
+		-- Draw the scope
+		surface.SetDrawColor(0, 0, 0, 255)
+		surface.SetTexture(surface.GetTextureID("jaanus/sniper_corner"))
+		surface.DrawTexturedRectRotated(self.ScopeTable.x1,self.ScopeTable.y1,self.ScopeTable.l,self.ScopeTable.l,270)
+		surface.DrawTexturedRectRotated(self.ScopeTable.x2,self.ScopeTable.y2,self.ScopeTable.l,self.ScopeTable.l,180)
+		surface.DrawTexturedRectRotated(self.ScopeTable.x3,self.ScopeTable.y3,self.ScopeTable.l,self.ScopeTable.l,90)
+		surface.DrawTexturedRectRotated(self.ScopeTable.x4,self.ScopeTable.y4,self.ScopeTable.l,self.ScopeTable.l,0)
+		-- Fill in everything else
+		surface.SetDrawColor(0,0,0,255)
+		surface.DrawRect(self.QuadTable.x1,self.QuadTable.y1,self.QuadTable.w1,self.QuadTable.h1)
+		surface.DrawRect(self.QuadTable.x2,self.QuadTable.y2,self.QuadTable.w2,self.QuadTable.h2)
+		surface.DrawRect(self.QuadTable.x3,self.QuadTable.y3,self.QuadTable.w3,self.QuadTable.h3)
+		surface.DrawRect(self.QuadTable.x4,self.QuadTable.y4,self.QuadTable.w4,self.QuadTable.h4)
+		local rotatick = CurTime()*90/5
+		surface.SetTexture(surface.GetTextureID("jaanus/rotatingthing"))
+		surface.SetDrawColor(0,0,0,120)
+		surface.DrawTexturedRectRotated(ScrW()/2, ScrH()/2,self.LensTable.w * 1.2, self.LensTable.h * 1.2, rotatick)
+	end
+end
