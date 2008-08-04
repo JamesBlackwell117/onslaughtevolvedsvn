4aa3c1d59c291c2cfe78d79e388aba5df4987570
ailiamorisato <ailiamorisato@5b65d7ca-4450-0410-b60c-19f04ccc024d>
2008-08-04 05:32:18 +0000

    fixed bugged shotgun combine and police

--- trunk/ onslaughtevolvedsvn/gamemode/shared.lua
+++ trunk/ onslaughtevolvedsvn/gamemode/shared.lua
@@ -86,8 +86,8 @@ NPCS = {}
 NPCS["npc_combine_s"] = {}
 NPCS["npc_combine_s"][1] =		{FLAGS = 403204, MONEY = 100, 	MODEL = "models/combine_soldier.mdl", 	  			KEYS = "tacticalvariant 1 additionalequipment weapon_smg1 model models/combine_soldier.mdl NumGrenades 999999 wakeradius 999999"}
 NPCS["npc_combine_s"][2] =		{FLAGS = 403204, MONEY = 140, 	MODEL = "models/combine_super_soldier.mdl", 		KEYS = "tacticalvariant 1 additionalequipment weapon_ar2 model models/combine_super_soldier.mdl wakeradius 999999"}
-NPCS["npc_combine_s"][3] =		{FLAGS = 402948, MONEY = 120, 	MODEL = "models/combine_soldier_prisonguard.mdl", 	KEYS = "tacticalvariant 1 additionalequipment weapon_shotgun model models/combine_soldier_prisonguard.mdl NumGrenades 999999 wakeradius 999999"}
-NPCS["npc_metropolice"]  = 		{FLAGS = 402948, MONEY = 50,  	MODEL = "models/police.mdl",			  			KEYS = "additionalequipment weapon_pistol"}
+NPCS["npc_combine_s"][3] =		{FLAGS = 403204, MONEY = 120, 	MODEL = "models/combine_soldier_prisonguard.mdl", 	KEYS = "tacticalvariant 1 additionalequipment weapon_shotgun model models/combine_soldier_prisonguard.mdl NumGrenades 999999 wakeradius 999999"}
+NPCS["npc_metropolice"]  = 		{FLAGS = 403204, MONEY = 50,  	MODEL = "models/police.mdl",			  			KEYS = "additionalequipment weapon_pistol"}
 NPCS["npc_hunter"]  = 			{FLAGS = 9984, 	 MONEY = 500, 	MODEL = "models/hunter.mdl"}
 NPCS["npc_manhack"]  = 			{FLAGS = 263940, MONEY = 50,  	MODEL = "models/manhack.mdl"}
 NPCS["npc_zombie"]  = 			{FLAGS = 1796, 	 MONEY = 75,  	MODEL = "models/zombie/classic.mdl"}
